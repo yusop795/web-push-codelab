370a49ca49c8b332cfbde2af2c44fc5ebdfe8534
yusop <yusop795@gmail.com>
2020-05-09 21:59:11 +0900

    알림 테스트

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -21,13 +21,15 @@
 
 'use strict';
 
-const applicationServerPublicKey = '<Your Public Key>';
+/* https://web-push-codelab.glitch.me */
+const applicationServerPublicKey = 'BCbzLYorVB3ZbO1Vqi2yqB_r8_HgAYbfKWddzinFCllFFvaRYHx_sQ3KJL_888yT_XX2Z9KJPb-itpqsoaVUBvk';
 
 const pushButton = document.querySelector('.js-push-btn');
 
 let isSubscribed = false;
 let swRegistration = null;
 
+/* UInt8Array로 변환  */
 function urlB64ToUint8Array(base64String) {
   const padding = '='.repeat((4 - base64String.length % 4) % 4);
   const base64 = (base64String + padding)
@@ -41,4 +43,119 @@ function urlB64ToUint8Array(base64String) {
     outputArray[i] = rawData.charCodeAt(i);
   }
   return outputArray;
+}
+
+/*
+이 코드는 현재 브라우저에서 서비스 워커와 푸시 메시지를 지원하는지 확인하며, 지원한다면 sw.js 파일을 등록합니다.
+*/
+if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+    .then(function (swReg) {
+      console.log('Service Worker is registered', swReg);
+
+      swRegistration = swReg;
+      /* 서비스 워커가 등록될 때 initialiseUI 실행 */
+      initialiseUI();
+    })
+    .catch(function (error) {
+      console.error('Service Worker Error', error);
+    });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+
+/* 구독 상태 여부를 판단해서 버튼을 활성화하거나 비활성화함 */
+function updateBtn() {
+  /* 사용자가 권한 요청을 차단할 경우
+    사용자가 권한을 차단하면 웹 앱이 권한 프롬프트를 다시 표시할 수 없고 사용자가 구독할 수 없게 되므로 사용자가 푸시 버튼을 사용할 수 없음
+    => 버튼을 영구적으로 비활성화하는 것이 최선
+  */
+  if (Notification.permission === 'denied') {
+    pushButton.textContent = 'Push Messaging Blocked.';
+    pushButton.disabled = true;
+    updateSubscriptionOnServer(null);
+    return;
+  }
+
+  if (isSubscribed) {
+    pushButton.textContent = 'Disable Push Messaging';
+  } else {
+    pushButton.textContent = 'Enable Push Messaging';
+  }
+
+  pushButton.disabled = false;
+}
+
+/* 사용자가 푸시 메시지를 구독하는가 */
+function subscribeUser() {
+  const applicationServerKey = urlB64ToUint8Array(applicationServerPublicKey);
+
+  /* 애플리케이션 서버의 공개 키와 userVisibleOnly: true 값을 반환 */
+  swRegistration.pushManager.subscribe({
+    /* userVisibleOnly는 기본적으로 푸시가 전송될 때마다 알림을 표시하도록 허용하는 것 */
+    userVisibleOnly: true,
+    applicationServerKey: applicationServerKey
+  })
+    .then(function (subscription) {
+      /* 두 가지가 완료된 것
+      사용자가 알림을 표시할 권한을 허용했습니다/브라우저가 PushSubscription 생성을 위한 세부 정보를 얻기 위해 푸시 서비스로 네트워크 요청을 보냈습니다.
+      */
+      console.log('User is subscribed:', subscription);
+
+      updateSubscriptionOnServer(subscription);
+
+      isSubscribed = true;
+
+      updateBtn();
+    })
+    .catch(function (err) {
+      console.log('Failed to subscribe the user: ', err);
+      updateBtn();
+    });
+}
+
+/* 사용자가 현재 구독 상태인지를 확인 */
+function initialiseUI() {
+  /* 사용자가 푸시 버튼을 클릭할 때 푸시를 구독하는 데 시간이 걸릴 수 있으니 버튼을 비활성화하여 구독하는 동안 사용자가 연속으로 버튼을 클릭할 수 없게 */
+  pushButton.addEventListener('click', function () {
+    pushButton.disabled = true;
+    if (isSubscribed) {
+      // TODO: Unsubscribe user
+    } else {
+      subscribeUser();
+    }
+  });
+  /* getSubscription() 구독이 있는 경우 
+  현재 구독으로 확인되는 프라미스를 반환하고 
+  그렇지 않으면 null을 반환하는 메서드 */
+  swRegistration.pushManager.getSubscription()
+    .then(function (subscription) {
+      isSubscribed = !(subscription === null);
+      if (isSubscribed) {
+        console.log('User IS subscribed.');
+      } else {
+        console.log('User is NOT subscribed.');
+      }
+
+      updateBtn();
+    });
+}
+
+/* 실제 앱에서 백으로 구독을 보내는 메시지 */
+function updateSubscriptionOnServer(subscription) {
+  // TODO: Send subscription to application server
+
+  const subscriptionJson = document.querySelector('.js-subscription-json');
+  const subscriptionDetails =
+    document.querySelector('.js-subscription-details');
+
+  if (subscription) {
+    subscriptionJson.textContent = JSON.stringify(subscription);
+    subscriptionDetails.classList.remove('is-invisible');
+  } else {
+    subscriptionDetails.classList.add('is-invisible');
+  }
 }
--- app/sw.js
+++ app/sw.js
@@ -1,22 +1,61 @@
+'use strict';
+
+
+/* 1. 트리거 시 보여질 알림 설정 */
 /*
-*
-*  Push Notifications codelab
-*  Copyright 2015 Google Inc. All rights reserved.
-*
-*  Licensed under the Apache License, Version 2.0 (the "License");
-*  you may not use this file except in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*      https://www.apache.org/licenses/LICENSE-2.0
-*
-*  Unless required by applicable law or agreed to in writing, software
-*  distributed under the License is distributed on an "AS IS" BASIS,
-*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-*  See the License for the specific language governing permissions and
-*  limitations under the License
-*
+self.addEventListener('push', ...);
+서비스 워커에 이벤트 리스너(아래의 코드)를 추가하여 
+서비스 워커에서 발생하는 푸시 이벤트를 수신할 수 있도록 합니다.
+
+self = 서비스 워커 자체를 참고, 서비스 워커에 이벤트 리스너 추가
+
+푸시 메시지 수신 -> 이벤트 리스너 실행 -> 등록 -> showNonfiction() 호출 -> 알림 생성
+
+showNonfiction()은 title과 option 객체를 넘겨받을 수 있습니다
+option =>  본문 메시지, 아이콘 및 배지 설정 가능
+
+event.waitUntil()
+프라미스를 취하며 브라우저는 전달된 프라미스가 확인될 때까지 서비스 워커를 활성화 및
+실행 상태로 유지할 것
+*/
+self.addEventListener('push', function (event) {
+    console.log('[Service Worker] Push Received.');
+    console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+
+    const title = 'Take me Home';
+    const options = {
+        body: '휴 많이 힘드네요 얼른 쉬고 싶어요!',
+        icon: 'https://t1.daumcdn.net/cfile/tistory/994BEF355CD0313D05',
+        badge: 'images/badge.png'
+    };
+
+    event.waitUntil(self.registration.showNotification(title, options));
+});
+
+/*
+아래 코드로 대체 가능
+
+const notificationPromise = self.registration.showNotification(title, options);
+event.waitUntil(notificationPromise);
 */
 
-/* eslint-env browser, serviceworker, es6 */
 
-'use strict';
+
+/* 2. 알림 클릭 시 발생될 일 정의 */
+/* notificationclick 리스너 추가 */
+/**
+ * 사용자가 알림 클릭 -> 클릭한 알림 닫기 -> notificationclick 이벤트 리스너 호출 -> waitUntil 활용하여 지정해 준 브라우저가 표시되기 전에는 브라우저가 서비스 워커를 종료하지 못하도록 설정 
+ * 
+ */
+self.addEventListener('notificationclick', function (event) {
+    console.log('[Service Worker] Notification click Received.');
+
+    event.notification.close();
+
+    /* https://www.youtube.com/watch?v=KO7S0JCec7Q */
+    /* https://www.youtube.com/watch?v=g8BV0rvbmOc */
+
+    event.waitUntil(
+        clients.openWindow('https://www.youtube.com/watch?v=ZWue6i_LRZ4')
+    );
+});
